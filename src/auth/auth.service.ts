7dff5eed82e6800d6975af08a34108dc489cc8d9
Rodrigo Carvalho <rodrigo010carvalho@gmail.com>
2025-09-17 16:57:45 -0300

    feat: implement signUp method in authService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   async signUp(SignUpDto: SignUpDto) {
-    // Lógica futura de registro
-    return 'Esta ação retorna um novo usuário';
+    const usuarioEncontrado = await this.prismaService.user.findMany({
+      where: {
+        email: SignUpDto.email,
+      },
+    });
+    if (usuarioEncontrado.length > 0) {
+      throw new ConflictException('Este e-mail já está em uso');
+    }
+    const hashedPassword = await bcrypt.hash(SignUpDto.password, 10);
+    return this.prismaService.user.create({
+      data: {
+        email: signUpDto.email,
+        name: signUpDto.name,
+        password: hashedPassword,
+        role: signUpDto.role,
+      },
+    });
   }
 
   async signIn(SignInDto: SignInDto) {
