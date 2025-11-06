6c9c85b03ed0a6648eaf38ffec4be338f2af9ca1
Rodrigo Carvalho <rodrigo010carvalho@gmail.com>
2025-11-06 17:50:17 -0300

    fix: AuthProvider.tsx null

--- frontend/src/app/AuthTokenProvider.tsx
+++ frontend/src/app/AuthTokenProvider.tsx
@@ -5,7 +5,7 @@ import { AuthProvider } from '@/contexts/AuthContext';
 import { cookies } from 'next/headers';
 
 export default async function AuthTokenProvider({ children }: { children: React.ReactNode }) {
-  const authToken = (await getCookie('authToken', { cookies })) || null;
+  const authToken = await getCookie('authToken', { cookies });
 
-  return <AuthProvider token={authToken}>{children}</AuthProvider>;
+  return <AuthProvider token={authToken === undefined ? null : authToken}>{children}</AuthProvider>;
 }
