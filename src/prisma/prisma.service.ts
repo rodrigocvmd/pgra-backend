cb24334aad6d84a831a1d40d722b9f019abad74f
Rodrigo Carvalho <rodrigo010carvalho@gmail.com>
2025-12-17 09:56:21 -0300

    fix: connection issues

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,28 +1,17 @@
 // Importe as classes necessárias
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    const url = process.env.DATABASE_URL;
-
-    super(
-      url
-        ? {
-            datasources: {
-              db: {
-                url: url.includes('?')
-                  ? `${url}&pgbouncer=true`
-                  : `${url}?pgbouncer=true`,
-              },
-            },
-          }
-        : undefined,
-    );
-  }
-
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
